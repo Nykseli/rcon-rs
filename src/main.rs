4dd437e68af611872afbdd634892ce1409b023c2
Miika Alikirri <nykseli@protonmail.com>
2024-02-24 23:47:15 +0200

    Send commands interactively

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+use std::io::{self, BufRead, Write};
 use std::process::exit;
 
 use connection::RemoteConnection;
 
 mod connection;
 mod packet;
+
+fn interactive(connection: &mut RemoteConnection, host: &str) {
+    let stdin = io::stdin();
+
+    loop {
+        print!("{host}> ");
+        io::stdout().flush().unwrap();
+        let mut command = String::new();
+        {
+            let len = stdin.lock().read_line(&mut command).unwrap();
+            if len == 0 {
+                break;
+            }
+
+            connection.send_command(command);
+        }
+    }
+
+    println!("Goodbye!");
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -14,5 +35,5 @@ fn main() {
 
     let mut con = RemoteConnection::from_address(&args[1]);
     con.authenticate(args[2].clone());
-    con.send_command("status".into());
+    interactive(&mut con, &args[1]);
 }
